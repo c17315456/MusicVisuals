946438a2f6f4fc1b56df3872638484a6d6ccd9c8
[c17315456] <[c17315456@mytudublin.ie]>
2020-05-19 00:57:59 +0100

    Tried to set the background but failed

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -3,6 +3,7 @@ package ie.tudublin;
 import example.CubeVisual;
 import example.MyVisual;
 import example.RotatingAudioBands;
+import ie.tudublin.Playit;
 
 public class Main
 {	
@@ -10,7 +11,7 @@ public class Main
 	public void startUI()
 	{
 		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new RotatingAudioBands());		
+        processing.core.PApplet.runSketch( a, new Playit());		
 	}
 
 	public static void main(String[] args)
--- java/src/ie/tudublin/Playit.java
+++ java/src/ie/tudublin/Playit.java
@@ -1,1 +1,25 @@
 package ie.tudublin;
+
+import processing.core.PImage;
+import processing.core.PApplet;
+
+public class Playit extends PApplet 
+{
+
+    PImage bkimg;
+
+    public void settings()
+    {
+        size(1100, 618);
+        // Use fullscreen instead of size to make your interface fullscreen
+		bkimg = loadImage("images/wallpaper.jpg");
+        //fullScreen(); 
+    }
+
+    public void draw()
+    {
+        background(bkimg);
+    }
+
+
+}
